5d3dcdaabd1a3cd42fe8b4c8864a3386d0b9a1e1
gabrielbalinca <balinca.gabriel.alin@gmail.com>
2024-07-10 18:42:08 +0300

    last version

--- README.md
+++ README.md
@@ -7,8 +7,8 @@
 * Software required :
   
   - Python 3 : https://www.python.org/downloads/
-  - Ollama : https://ollama.com/download
- 
+  - Ollama : https://ollama.com/download 
+
 * For installing dependencies :
 ```
 pip install -r src/requirements.txt
@@ -18,9 +18,9 @@ ollama pull llama3
 # Run
 
 * Make sure all the files for training the AI are in ```train``` folder
+* Question will be written in the ```tests/input.md``` and the answear will be in the ```tests/output.md```
 * Run the app using this command :
-
 ```
-streamlit src/app.py
+python src/app.py
 ```
 
--- src/app.py
+++ src/app.py
@@ -1,47 +1,43 @@
 import ollama
-import docx
 import os
-import streamlit
+import markdown
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders.word_document import UnstructuredWordDocumentLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OllamaEmbeddings
 
 # 1. Load data from docx files
-directory = os.fsencode("./train")
-def getText():
-    for encodedFile in os.listdir(directory):
-        file = os.fsdecode(encodedFile)
-        if file.endswith(".docx"): 
-            doc = docx.Document(file)
-            fullText = []
-            for para in doc.paragraphs:
-                fullText.append(para.text)
-            continue
-        else:
-            continue
-    return '\n'.join(fullText)
+directory = os.path.abspath('train')
 
-loader = TextLoader(getText())
+loader = DirectoryLoader(
+    directory, 
+    glob = "*.docx",
+    loader_cls=UnstructuredWordDocumentLoader
+)
+
 docs = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
 
-# 2. Load model
-embeddings = OllamaEmbeddings(model="llama3")
-vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
-
-# 3. Prompt
+# 2. Prompt
 def prompt(question, context):
+    model='llama3'
     formatted_prompt = f"Question: {question}\n\nContext: {context}"
-    response = ollama.chat(model='llama3', 
-                           messages=[{
-                               'role': 'user', 
-                               'content': formatted_prompt}])
+    messages=[{'role': 'user', 'content': formatted_prompt}]
+    response = ollama.chat(
+        model='llama3', 
+        messages=[{
+            'role': 'user', 
+            'content': formatted_prompt}]
+        )
+
     return response['message']['content']
 
-# 4. RAG
+# 3. setup (retrival augumented generation)
+embeddings = OllamaEmbeddings(model="llama3")
+vectorstore = Chroma.from_documents(splits, embeddings)
 retriever = vectorstore.as_retriever()
 def combine_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
@@ -50,8 +46,11 @@ def rag_chain(question):
     formatted_context = combine_docs(retrieved_docs)
     return prompt(question, formatted_context)
 
-# 5. Streamlit
-streamlit.title('app.py')
-result=streamlit.text_input("Ask your question")
-if result:
-    streamlit.write(prompt(result))
+# 
+with open('../tests/input.md', 'r') as f:
+    markdown_text = f.read()
+
+input = markdown.markdown(markdown_text)
+
+with open('../tests/output.md', 'w') as f:
+    f.write(rag_chain(input))
